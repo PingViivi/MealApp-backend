ddae1905c4afea8505fafb8d406846c0f8e35a8c
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2023-11-13 18:06:14 +0200

    Recipes by meal added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 //const apiUrl = `https://themealdb.com/api/json/v1/1/`;
 
-// Fetch meal categories
+// GET meal categories
 app.get('/api/categories', async (req, res) => {
   const categoriesUrl = `https://themealdb.com/api/json/v1/1/categories.php`;
   try {
@@ -17,25 +17,38 @@ app.get('/api/categories', async (req, res) => {
     res.status(200).json({ categories });
 
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch data" + error })
+    res.status(500).json({ message: "Failed to fetch data " + error })
   }
 });
 
-// Filter by category
-const category = 'Seafood'
-app.get(`/api/category/${category}`, async (req, res) => {
-  const categoriesUrl = `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+// GET meals by category
+app.get('/api/categories/:category', async (req, res) => { 
+  const category = req.params.category
+  const categoryUrl = `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+
   try {
-    const response = await fetch(categoriesUrl);
+    const response = await fetch(categoryUrl);
     const data = await response.json();
     const meals = data.meals
     res.status(200).json({ meals });
-
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch data" + error })
+    res.status(500).json({ message: "Failed to fetch data " + error })
   }
-});
+}); 
 
+// GET recipes by meal
+app.get('/api/meals/:id', async (req, res) => {
+  const id = req.params.id
+  const mealUrl = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  try {
+    const response = await fetch(mealUrl);
+    const data = await response.json();
+    const recipe = data.meals
+    res.status(200).json({ recipe });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch data " + error})
+  }
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
