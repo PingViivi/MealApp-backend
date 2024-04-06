6e086aa02e959a5b0108b9ddf0cee695085345f9
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2024-04-06 11:56:13 +0300

    Single recipe route added

--- src/routes-db.js
+++ src/routes-db.js
@@ -15,6 +15,28 @@ router.get('/myrecipes', async (req, res) => {
     }
 });
 
+router.get('/myrecipes/:recipeId', async (req, res) => {
+  const { recipeId } = req.params;
+
+  try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM recipes WHERE id = $1', [recipeId]);
+
+      if (result.rows.length === 0) {
+          // If no recipe is found with the provided ID, return a 404 Not Found response
+          res.status(404).send('Recipe not found');
+      } else {
+          // If a recipe is found, return it
+          res.json(result.rows[0]);
+      }
+
+      client.release();
+  } catch (err) {
+      console.error('Error executing query', err);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
 // const Recipe = sequelize.define('Recipe', {
 //   name: DataTypes.STRING,
 //   ingredients: DataTypes.TEXT,
@@ -24,16 +46,20 @@ router.get('/myrecipes', async (req, res) => {
   // Define a route to handle saving recipes
   router.post('/myrecipes', async (req, res) => {
     try {
-      const { name } = req.body;
-      const data = {name:name}
-      console.debug(data)
+      const { name, category, duration, recipeYield, description, area, image, recipeIngredient, recipeInstructions } = req.body;
+      
+      // const data = {name:name}
+      // console.debug(data)
       
       if (!name) {
         throw new Error("Name field is missing in the request body");
       }
       // Insert the recipe into the database
       const client = await pool.connect();
-      const result = await client.query('INSERT INTO recipes (name) VALUES ($1) RETURNING *', [name]);
+      const result = await client.query(
+        'INSERT INTO recipes (name, category, duration, recipeyield, description, area, image, recipeingredient, recipeinstructions) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+        [name, category, duration, recipeYield, description, area, image, recipeIngredient, recipeInstructions]
+    );
       client.release();
 
       res.status(201).json(result.rows[0]); // Return the saved recipe
